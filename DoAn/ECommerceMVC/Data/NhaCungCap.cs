9bb10c9758f7be50ce5d64d838d5ab2ff2c5b5a7
anhphuong204 <ahah200419@gmail.com>
2025-02-10 18:01:49 +0700

    first commit

---
+++ DoAn/ECommerceMVC/Data/NhaCungCap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ECommerceMVC.Data;
+
+public partial class NhaCungCap
+{
+    public string MaNcc { get; set; } = null!;
+
+    public string TenNCC { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string? DienThoai { get; set; }
+
+    public string? DiaChi { get; set; }
+
+    public string? MoTa { get; set; }
+
+    public virtual ICollection<HangHoa> HangHoas { get; set; } = new List<HangHoa>();
+}
